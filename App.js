fd0abd3bad9e72dee04ad772b7812a0b1c56a37d
Randy Ortiz <Randyortiz28@gmail.com>
2021-06-28 13:20:07 -0400

    Added Material-UI delete icon on todoItems

--- App.js
+++ App.js
@@ -64,13 +64,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
   content: {
     padding: 40,
+    flex: 1,
   },
   list: {
+    flex: 1,
     marginTop: 20,
   },
 })
